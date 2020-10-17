f3eed2dff74260f86429b68508ae4f86793bbbb3
Michael Chan <michchandev@gmail.com>
2020-10-17 15:36:36 +0800

    Change source code directory name from 'lib' to 'src'

--- bin/world-indices-monitor-backend.ts
+++ bin/world-indices-monitor-backend.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { WorldIndicesMonitorBackendStack } from '../lib/world-indices-monitor-backend-stack';
+import { WorldIndicesMonitorBackendStack } from '../src/world-indices-monitor-backend-stack';
 
 const app = new cdk.App();
 new WorldIndicesMonitorBackendStack(app, 'WorldIndicesMonitorBackendStack');
--- test/world-indices-monitor-backend.test.ts
+++ test/world-indices-monitor-backend.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as WorldIndicesMonitorBackend from '../lib/world-indices-monitor-backend-stack';
+import * as WorldIndicesMonitorBackend from '../src/world-indices-monitor-backend-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
