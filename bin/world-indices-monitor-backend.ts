1680299c2aebcea99124f73ffccaa0dc1f067f78
Michael Chan <michchandev@gmail.com>
2020-10-17 13:54:13 +0800

    Initial commit

---
+++ bin/world-indices-monitor-backend.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { WorldIndicesMonitorBackendStack } from '../lib/world-indices-monitor-backend-stack';
+
+const app = new cdk.App();
+new WorldIndicesMonitorBackendStack(app, 'WorldIndicesMonitorBackendStack');
---
+++ test/world-indices-monitor-backend.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as WorldIndicesMonitorBackend from '../lib/world-indices-monitor-backend-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new WorldIndicesMonitorBackend.WorldIndicesMonitorBackendStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
