f8a3e370f69087294ac9e2759aed7f589d9921c3
Michael Chan <michchandev@gmail.com>
2020-10-25 21:27:16 +0800

    Log

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,5 +2,6 @@ import * as cdk from '@aws-cdk/core'
 
 function construct (scope: cdk.Construct): void {
   // CDK Construct of api service goes here
+  console.log(scope)
 }
 export default { construct }
--- src/services/cron/cron.ts
+++ src/services/cron/cron.ts
@@ -2,5 +2,6 @@ import * as cdk from '@aws-cdk/core'
 
 function construct (scope: cdk.Construct): void {
   // CDK Construct of cron service goes here
+  console.log(scope)
 }
 export default { construct }
