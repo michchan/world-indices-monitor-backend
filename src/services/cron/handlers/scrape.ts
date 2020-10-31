8634fc89d20c954e8886fe9253148c76f17813bb
Michael Chan <michchandev@gmail.com>
2020-10-31 21:40:59 +0800

    Remove unnecessary try-catch block

--- src/services/cron/handlers/scrape.ts
+++ src/services/cron/handlers/scrape.ts
@@ -1,9 +1,5 @@
 import { ScheduledHandler } from 'aws-lambda'
 
-export const handler: ScheduledHandler = (event, context, callback) => {
-  try {
-    console.log('I am running!')
-  } catch (error) {
-    callback(error)
-  }
+export const handler: ScheduledHandler = () => {
+  console.log('I am running!')
 }
