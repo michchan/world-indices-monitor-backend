011906da83232492e22054f22e972071d63519e9
Michael Chan <michchandev@gmail.com>
2020-11-04 10:53:30 +0800

    Initialize page session of scraper

--- src/services/cron/handlers/scrape.ts
+++ src/services/cron/handlers/scrape.ts
@@ -1,5 +1,29 @@
 import { ScheduledHandler } from 'aws-lambda'
+import launchPuppeteerBrowserSession, {
+  GetDataWithPage,
+} from 'simply-utils/dist/scraping/launchPuppeteerBrowserSession'
+import IndexPriceRecord from 'src/models/IndexPriceRecord.type'
 
-export const handler: ScheduledHandler = () => {
-  console.log('I am running!')
+const PAGE_URL = 'https://finance.yahoo.com/world-indices/'
+const getDataFromPage: GetDataWithPage<IndexPriceRecord[]> = async page => {
+  // Navigate to the page we want to scrape
+  await page.goto(PAGE_URL)
+  // Wait for some element to be rendered completely
+  await page.waitForSelector('.yfinlist-table > tbody > tr:last-child > td:last-child')
+  return page.evaluate((): IndexPriceRecord[] => {
+    const tableRows = document.querySelectorAll('.yfinlist-table > tbody > tr')
+    return [{
+      symbol: '',
+      name: '',
+      lastPrice: tableRows.length,
+      change: 0,
+      changeRate: 0,
+      time: '',
+    }]
+  })
+}
+
+export const handler: ScheduledHandler = async () => {
+  const results = await launchPuppeteerBrowserSession([getDataFromPage])
+  console.log(JSON.stringify(results, null, 2))
 }
