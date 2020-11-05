16c69d56e3dae7c3e271a2c98c67ed2c1af7d8a9
Michael Chan <michchandev@gmail.com>
2020-11-05 10:58:55 +0800

    Implement scraper logic

--- src/services/cron/handlers/scrape.ts
+++ src/services/cron/handlers/scrape.ts
@@ -11,15 +11,31 @@ const getDataFromPage: GetDataWithPage<IndexPriceRecord[]> = async page => {
   // Wait for some element to be rendered completely
   await page.waitForSelector('.yfinlist-table > tbody > tr:last-child > td:last-child')
   return page.evaluate((): IndexPriceRecord[] => {
-    const tableRows = document.querySelectorAll('.yfinlist-table > tbody > tr')
-    return [{
-      symbol: '',
-      name: '',
-      lastPrice: tableRows.length,
-      change: 0,
-      changeRate: 0,
-      time: '',
-    }]
+    const tableRows: NodeListOf<HTMLTableRowElement> = document.querySelectorAll('.yfinlist-table > tbody > tr')
+    /**
+     * We have to put this inside the callback function of page.evaluate,
+     * because it need to be in the virtual client-side scope.
+     */
+    const parseNum = (text: string): number => parseFloat(text.replace(/\+|,|%/g, ''))
+
+    return Array.from(tableRows).map(tableRow => {
+      // Tell typescript that it is a list of data cells
+      const cols = tableRow.children as HTMLCollectionOf<HTMLTableDataCellElement>
+      const symbol = cols[0].innerText
+      const name = cols[1].innerText
+      const lastPrice = parseNum(cols[2].innerText)
+      const change = parseNum(cols[3].innerText)
+      const changeRate = parseNum(cols[4].innerText)
+      const time = new Date().toISOString()
+      return {
+        symbol,
+        name,
+        lastPrice,
+        change,
+        changeRate,
+        time,
+      }
+    })
   })
 }
 
